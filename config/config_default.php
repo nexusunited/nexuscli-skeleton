19f8204c9bfe28aed5eed3220e730e4f0a49a361
Mike Bertram <contact@mibexx.de>
2018-07-10 09:27:34 +0200

    * Development autocomplete + Dataprovider (DTO)

--- config/config_default.php
+++ config/config_default.php
@@ -1,5 +1,15 @@
 <?php
 
 use Xervice\Core\CoreConfig;
+use Xervice\DataProvider\DataProviderConfig;
+use Xervice\Development\DevelopmentConfig;
 
 $config[CoreConfig::PROJECT_LAYER_NAMESPACE] = 'Nexus';
+
+$config[DevelopmentConfig::GENERATED_PATH] = dirname(__DIR__) . '/_Generated/Ide';
+$config[DataProviderConfig::DATA_PROVIDER_GENERATED_PATH] = dirname(__DIR__) . '/_Generated';
+$config[DataProviderConfig::DATA_PROVIDER_PATHS] = [
+    dirname(__DIR__) . '/src/Nexus/*/Schema/',
+    dirname(__DIR__) . '/vendor/xervice/*/src/Xervice/*/Schema/',
+    dirname(__DIR__) . '/vendor/nexusnetsoftgmbh/*/src/*/*/Schema/'
+];
